39e7b26c70a69439aca4a8bb8c36742973d257f7
nt-kansya12 <153280158+nt-kansya12@users.noreply.github.com>
2023-12-18 19:43:58 +0700

    Update blacklist.py

--- MukeshRobot/modules/blacklist.py
+++ MukeshRobot/modules/blacklist.py
@@ -93,7 +93,7 @@ def add_blacklist(update, context):
             sql.add_to_blacklist(chat_id, trigger.lower())
 
         if len(to_blacklist) == 1:
-            msg.reply_text(tld(chat.id, "Added <code>{}</code> to the blacklist in <b>{}</b>!").format(html.escape(to_blacklist[0]), chat_name), parse_mode=ParseMode.HTML)
+            msg.reply_text(chat.id, "Added <code>{}</code> to the blacklist in <b>{}</b>!".format(html.escape(to_blacklist[0]), chat_name), parse_mode=ParseMode.HTML)
 
         else:
             msg.reply_text(tld(chat.id, "Added <code>{}</code> to the blacklist in <b>{}</b>!").format(len(to_blacklist)), chat_name, parse_mode=ParseMode.HTML)
